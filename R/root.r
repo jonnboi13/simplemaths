4af7df0e25b2c2ed85f6e9715a9ae9e1aede6059
jonnboi13 <jonnyboi13@byui.edu>
2026-03-03 16:01:25 -0700

    testing ONLY lintr

--- R/root.r
+++ R/root.r
@@ -1,6 +1,6 @@
 #' An example root function
 #' @param num is the number to take the root of
-#' @param x is the root degree (2 for square root, 3 for cubed root, etc.)
+#' @param n is the root degree (2 for square root, 3 for cubed root, etc.)
 #' @examples root(25, 2)
 #' @export  
 root <- function(num, n) {
